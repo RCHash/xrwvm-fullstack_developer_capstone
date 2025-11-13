da49a4cbdf65b63f77a3c80e0da4751aedb6c51b
RCHash <94483717+RCHash@users.noreply.github.com>
2025-11-13 09:08:24 -0500

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,6 +1,9 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # Register your models here.
 
@@ -9,5 +12,3 @@
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
-
-# Register models here
